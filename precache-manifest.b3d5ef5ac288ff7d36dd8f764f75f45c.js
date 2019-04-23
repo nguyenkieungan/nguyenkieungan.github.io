bb71ed5a3b9f1b6c31feef511c7cda3af9e20515
nguyenkieungan <nguyen.kieu.ngan@framgia.com>
2019-04-23 10:27:08 +0700

    Updates

---
+++ precache-manifest.b3d5ef5ac288ff7d36dd8f764f75f45c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a0a3a5d27facd1fedf4e",
+    "url": "/static/js/main.a0a3a5d2.chunk.js"
+  },
+  {
+    "revision": "958d0559fff1c67c7321",
+    "url": "/static/js/2.958d0559.chunk.js"
+  },
+  {
+    "revision": "a0a3a5d27facd1fedf4e",
+    "url": "/static/css/main.769061b0.chunk.css"
+  },
+  {
+    "revision": "151700e6d3c9e621aa55e37bf28adc71",
+    "url": "/index.html"
+  }
+];
